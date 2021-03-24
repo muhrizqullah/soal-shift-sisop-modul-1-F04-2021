ec49211c12857403fac410243cbb7fa3f3bfba81
fiansyahm <73706605+fiansyahm@users.noreply.github.com>
2021-03-24 22:02:22 +0700

    Update soal2_generate_laporan_ihir_shisop.sh

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -9,3 +9,43 @@ awk 'BEGIN { FS="\t"; OFS=","; ORS="\r\n" } { if(($18-$21) > 0 ) if( ($21/($18-$
 #!/bin/bash
 
 awk 'BEGIN { print "Daftar nama customer di Albuquerque pada tahun 2017 antara lain:" ; FS="\t"; OFS=","; ORS="\r\n"} {split($3, tglArr, "-"); if (tglArr[3] == 17 && $10 == "Albuquerque") print $7  }  ' /home/misdinar/Laporan-TokoShiSop.tsv | uniq > /home/misdinar/hasil.txt
+
+#no 2.d
+#!/bin/bash
+South=0
+West=0
+East=0
+Central=0;
+top=10000000000000000000000000000000000
+wilayah="a"
+
+awk 'BEGIN { FS="\t"; OFS=","; ORS="\r\n" } {
+if(top>South){
+  wilayah="South"
+  top=South
+}
+else if(top>West){
+  wilayah="West"
+  top=West
+}
+else if(top>East){
+  wilayah="East"
+  top=East
+}
+else if(top>Central){
+ wilayah="Central"
+ top=Central
+}
+else if($13=="South")
+    South=South+$21;
+else if($13=="West")
+    West=West+$21;
+else if($13=="East")
+     East=East+$21;
+else if($13=="Central")
+      Central=Central+$21;
+}
+
+END{
+print "Wilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah " wilayah " dengan total keuntungan " top
+}' ./Laporan-TokoShiSop.tsv  > /mnt/c/Users/ASUS/Downloads/sisop/hasil1.txt
