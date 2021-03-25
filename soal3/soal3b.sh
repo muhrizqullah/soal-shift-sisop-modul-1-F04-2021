daf72c6717d7cd0c2b798be3b80a30990e798959
fiansyahm <73706605+fiansyahm@users.noreply.github.com>
2021-03-25 17:32:13 +0700

    Update soal3b.sh

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-mkdir /home/fiansyahm/file/$(date +%d-%m-%Y)
+mkdir /home/fiansyahm/$(date +%d-%m-%Y)
 
 count=1
 while [ $count -le 10 ]
 do
-        wget https://loremflickr.com/320/240/kitten -O Koleksi_0$count  2>>/home/fiansyahm/file/Foto.log
-        mv Koleksi_0$count /home/fiansyahm/file/$(date +%d-%m-%Y)
+        wget https://loremflickr.com/320/240/kitten -O Koleksi_0$count  2>>/home/fiansyahm/Foto.log
+        mv Koleksi_0$count /home/fiansyahm/$(date +%d-%m-%Y)
         ((count++))
 done
 
 count1=11
 while [ $count1 -le 23 ]
 do
-         wget https://loremflickr.com/320/240/kitten -O Koleksi_$count1  2>>/home/fiansyahm/file/Foto.log
-         mv Koleksi_$count1 /home/fiansyahm/file/$(date +%d-%m-%Y)
+         wget https://loremflickr.com/320/240/kitten -O Koleksi_$count1  2>>/home/fiansyahm/Foto.log
+         mv Koleksi_$count1 /home/fiansyahm/$(date +%d-%m-%Y)
          ((count1++))
 done
 
-mv /home/fiansyahm/file/Foto.log /home/fiansyahm/file/$(date +%d-%m-%Y)
+mv /home/fiansyahm/Foto.log /home/fiansyahm/$(date +%d-%m-%Y)
 
 declare -A arr
 shopt -s globstar
