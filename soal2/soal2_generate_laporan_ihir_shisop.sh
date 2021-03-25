d86750436e5ddb6e0076ae4c8229cb997942ab0a
Muhammad Rizqullah Akbar <muhrizqullah@icloud.com>
2021-03-25 11:14:47 +0700

    Add hasil.txt files. Satuin output nomor 2

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -1,14 +1,19 @@
+#no 2.a
 #!/bin/bash
+
 max=0
 ID=0
 
-awk 'BEGIN { FS="\t"; OFS=","; ORS="\r\n" } { if(($18-$21) > 0 ) if( ($21/($18-$21)) > max) max=$21/($18-$21);ID=$1} END{print "Transaksi terakhir dengan profit percentage terbesar yaitu " ID " dengan persentase " max*100  "%."}' ./Laporan-TokoShiSop.tsv  > /mnt/c/Users/ASUS/Downloads/sisop/hasil.txt
+awk 'BEGIN { FS="\t"; OFS=","; ORS="\r\n" } 
+{ if(($18-$21) > 0 ) if( ($21/($18-$21)) > max) max=$21/($18-$21);ID=$1} 
+END{print "Transaksi terakhir dengan profit percentage terbesar yaitu " ID "\ndengan persentase " max*100  "%."}' /home/gretzy/soal-shift-1-local/Laporan-TokoShiSop.tsv > /home/gretzy/soal-shift-sisop-modul-1-F04-2021/soal2/hasil.txt
 
 
 #no 2.b
 #!/bin/bash
 
-awk 'BEGIN { print "Daftar nama customer di Albuquerque pada tahun 2017 antara lain:" ; FS="\t"; OFS=","; ORS="\r\n"} {split($3, tglArr, "-"); if (tglArr[3] == 17 && $10 == "Albuquerque") print $7  }  ' /home/misdinar/Laporan-TokoShiSop.tsv | uniq > /home/misdinar/hasil.txt
+awk 'BEGIN { print "\nDaftar nama customer di Albuquerque pada tahun 2017 antara lain:" ; FS="\t"; OFS=","; ORS="\r\n"} 
+{split($3, tglArr, "-"); if (tglArr[3] == 17 && $10 == "Albuquerque") print $7  }  ' /home/gretzy/soal-shift-1-local/Laporan-TokoShiSop.tsv | uniq >> /home/gretzy/soal-shift-sisop-modul-1-F04-2021/soal2/hasil.txt
 
 
 #no 2.c
@@ -44,9 +49,8 @@ else if(HomeOffice<Consumer&&HomeOffice<Corporate){
         segment="Home Office"
         top=HomeOffice
         }
-print "Tipe segmen customer yang penjualannya paling sedikit adalah " segment " dengan " top " transaksi."
-}' ./Laporan-TokoShiSop.tsv  > /mnt/c/Users/ASUS/Downloads/sisop/hasil2.txt
-
+print "\nTipe segmen customer yang penjualannya paling sedikit adalah " segment "\ndengan " top " transaksi."
+}' /home/gretzy/soal-shift-1-local/Laporan-TokoShiSop.tsv >> /home/gretzy/soal-shift-sisop-modul-1-F04-2021/soal2/hasil.txt
 
 
 
@@ -87,5 +91,5 @@ else if($13=="Central")
 }
 
 END{
-print "Wilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah " wilayah " dengan total keuntungan " top
-}' ./Laporan-TokoShiSop.tsv  > /mnt/c/Users/ASUS/Downloads/sisop/hasil1.txt
+print "\nWilayah bagian (region) yang memiliki total keuntungan (profit) yang\npaling sedikit adalah " wilayah " dengan total keuntungan " top
+}' /home/gretzy/soal-shift-1-local/Laporan-TokoShiSop.tsv >> /home/gretzy/soal-shift-sisop-modul-1-F04-2021/soal2/hasil.txt
