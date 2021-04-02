0b2dd34e6c87511c3aa7779bb0813aee67828a29
Misdinar <55637655+Misdinar@users.noreply.github.com>
2021-04-02 11:13:26 -0700

    ANJAY KELAR 1D

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -18,3 +18,18 @@ grep -o '(.*)' /home/gretzy/listerror.log | sort | uniq -c
 
 grep -o 'INFO.*' /home/gretzy/soal-shift-1-local/soal1/syslog.log > /home/gretzy/listinfo.log
 grep -o '(.*)' /home/gretzy/listinfo.log | sort | uniq -c
+
+#1d
+echo "Error,Count" > error_message.csv
+errCount=`grep -c 'Time.*information' /home/misdinar/Sisop1/modul1/soal1/syslog.log | uniq `
+printf "Timeout while retrieving information,$errCount\n" >> error_message.csv
+errCount=`grep -c 'Connect.*failed' /home/misdinar/Sisop1/modul1/soal1/syslog.log | uniq`
+printf "Connection to DB failed,$errCount\n" >> error_message.csv
+errCount=`grep -c 'Tried.*ticket' /home/misdinar/Sisop1/modul1/soal1/syslog.log | uniq`
+printf "Tried to add information to closed ticket,$errCount\n" >> error_message.csv
+errCount=`grep -c 'Permission.*ticket' /home/misdinar/Sisop1/modul1/soal1/syslog.log | uniq`
+printf "Permission denied while closing ticket,$errCount\n" >> error_message.csv
+errCount=`grep -c 'The.*updating' /home/misdinar/Sisop1/modul1/soal1/syslog.log | uniq`
+printf "The ticket was modified while updating,$errCount\n" >> error_message.csv
+errCount=`grep -c 'Ticket.*exist' /home/misdinar/Sisop1/modul1/soal1/syslog.log | uniq`
+printf "Ticket doesn't exist,$errCount\n" >> error_message.csv
